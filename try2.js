091f74e879e6fd2e2c751f20dcbf9bdf96ff9901
Ben Prothe <beprothe@gmail.com>
2020-02-02 13:58:05 -0500

    added fresh js sheet and tried a new approach. result is cleaner

--- try2.js
+++ try2.js
@@ -17,21 +17,58 @@ const errorBox = document.querySelector('#error-box')
 let currentTime = moment()
 
 
-
 // SUMBIT EVENT:
 
 parkingForm.addEventListener ('submit', function(e){
   let errorMessages = []
-  e.preventDefault()
+
+  // e.preventDefault()
+
 // CHECK EMPTIES
   for (let element of formElements) {
     if (element.value === "") {
       markInvalid(element)
-      element.value = "*REQUIRED FIELD*"
+      element.parentElement.lastElementChild.innerText = "*"
+      element.parentElement.lastElementChild.style = "color: red;"
+      if (!errorMessages.includes ("* Required Field")){
+        errorMessages.push("* Required Field")
+      } 
     } else {
       markValid(element)
+      element.parentElement.lastElementChild.innerText = ""
     }
   }
+// CARYEAR CHECKS
+
+  if(isNaN(carYear.value)){
+    markInvalid(carYear);
+    errorMessages.push("Car year must be a number")
+  } else if (carYear.value<1900){ //EVAL RELATIVE TO 1900
+    markInvalid(carYear)
+    errorMessages.push("Car year must be after 1900")
+  } else if (currentTime.isBefore(carYear.value)){ //EVAL FUTURE
+    markInvalid(carYear)
+    errorMessages.push("Car year cannot be in future")
+  }
+  else {
+    carYear.parentElement.parentElement.classList.add ("input-valid")
+  }
+
+// STARTDATE CHECK
+  if(currentTime.isAfter(startDate.value)){
+    markInvalid(startDate)
+    errorMessages.push("Start date must be a future date")
+  }
+
+// CHECK DAY
+  if (isNaN(days.value)){
+    markInvalid(days)
+    errorMessages.push("Number of days must be a numer")
+  } else if (days.value<1 || days.value>30){
+    markInvalid(days)
+    errorMessages.push("Number of days must be between 1 and 30")
+  }
+
 // CVV CHECK:
   if (cvv.value.length !== 3) {
     errorMessages.push("CVV must be exactly 3 digits")
@@ -42,7 +79,7 @@ parkingForm.addEventListener ('submit', function(e){
   
   if (errorMessages.length > 0) {
     e.preventDefault()
-    errorBox.innerText = errorMessages.join(', ')
+    errorBox.innerText = errorMessages.join('\n')
   } else {
     alert("Form Submitted")
   }
@@ -51,12 +88,12 @@ parkingForm.addEventListener ('submit', function(e){
 // VALID / INVALID FUNCTIONS:
 
 function markInvalid(element) {
-  formInvalid = true;
+  // formInvalid = true;
   errorBox.classList.remove("alert")
   errorBox.classList.remove("alert-danger")
   errorBox.classList.add("alert")
   errorBox.classList.add("alert-danger")
-  if (element.parentElement.className == "input-group") {
+  if (element.parentElement.className === "input-group") {
     element.parentElement.parentElement.classList.add("input-invalid")
     element.parentElement.parentElement.classList.remove("input-valid")
   } else {
@@ -66,7 +103,7 @@ function markInvalid(element) {
 }
 
 function markValid(element) {
-  if (element.parentElement.className == "input-group") {
+  if (element.parentElement.className === "input-group") {
     element.parentElement.parentElement.classList.add("input-valid")
     element.parentElement.parentElement.classList.remove("input-invalid")
   } else {
@@ -75,13 +112,6 @@ function markValid(element) {
   }
 }
 
-// INDIVIDUAL CHECKS:
-
-function checkCvv (){
-  if (cvv.value.length !== 3) {
-    errorMessages.push("CVV must be exactly 3 digits")
-    markInvalid(cvv)
-  } else {
-    markValid(cvv)
-  }
+function calculateCharges() {
+  let endDate = moment().add('days', days.value)
 }
